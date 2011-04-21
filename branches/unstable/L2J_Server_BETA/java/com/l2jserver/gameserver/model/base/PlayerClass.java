ce8e4e3798aa2bc44c73280c112877a4d51f764a
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-04-21 09:29:20 +0000

    SYNC: CORE r4579 BETA: Fix for ticket #5391 (thanks nik)

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/base/PlayerClass.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/base/PlayerClass.java
@@ -305,7 +305,7 @@ public enum PlayerClass
 					else
 						subclasses.removeAll(EnumSet.of(maleSoulbreaker));
 				}
-				if (player.getTotalSubClasses() < 2)
+				if (player.getTotalSubClasses() <= 2)
 					subclasses.removeAll(EnumSet.of(inspector));
 			}
 		}
