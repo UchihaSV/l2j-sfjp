bc3b25d0180efac13d5e725d39e856b35d1ec496
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-06-03 14:03:21 +0000

    昼夜の切り替わりが１時間ずれるバグを修正. 夜 00:00～05:59 (ゲーム内時間) 昼 06:00～23:59 (ゲーム内時間)

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/GameTimeController.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/GameTimeController.java
@@ -228,7 +228,11 @@ public class GameTimeController
 		
 		public void run()
 		{
+if (com.l2jserver.Config.FIX_DAY_NIGHT_TIME) {{
+			h = (getGameTime() + 29) / 60 % 24; // Time in hour
+}} else {{
 			h = (getGameTime() / 60) % 24; // Time in hour
+}}
 			tempIsNight = (h < 6);
 			
 			if (tempIsNight != _isNight)
