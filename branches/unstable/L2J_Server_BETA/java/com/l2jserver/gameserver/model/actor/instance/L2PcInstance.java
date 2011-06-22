d78d3161e0f0a98a15954868a884e7ad808f3042
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-06-22 16:20:18 +0000

    SYNC: CORE r4684 / Zoey76 BETA: Fixes for following tickets:  * #5495 and #5477, NPE@SummonEffectsTable.java:45  * #5514, players shouldn't be able to enchant skills in stance mode.  * #5507, NPE if SubClassSkills quests is null, reloaded scripts or not updated datapack...

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
@@ -14392,7 +14392,7 @@ public final class L2PcInstance extends L2Playable
 	{
 		if (isLocked())
 			return false;
-		if (isTransformed())
+		if (isTransformed() || isInStance())
 			return false;
 		if (AttackStanceTaskManager.getInstance().getAttackStanceTask(this))
 			return false;
