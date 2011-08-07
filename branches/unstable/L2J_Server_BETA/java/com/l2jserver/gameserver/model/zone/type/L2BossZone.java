96f2dec8b3bfbe5c5dbcb4edd7c73ff54906a780
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-08-07 12:00:14 +0000

    SYNC: CORE r4801 / Nik BETA: L2TIntObjectHashMap at GameTimeController, L2Manor, L2Attackable, L2ZoneType. Can't think of any more places where i can use this, except knownlist (im still testing this rework).

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/zone/type/L2BossZone.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/zone/type/L2BossZone.java
@@ -214,7 +214,7 @@ public class L2BossZone extends L2ZoneType
 				{
 					_raidList.clear();
 					int count = 0;
-					for (L2Character obj : getCharactersInside().values())
+					for (L2Character obj : getCharactersInsideArray())
 					{
 						if (obj == null)
 							continue;
@@ -299,7 +299,7 @@ public class L2BossZone extends L2ZoneType
 		if (_characterList.isEmpty())
 			return;
 		
-		for (L2Character character : _characterList.values())
+		for (L2Character character : getCharactersInsideArray())
 		{
 			if (character instanceof L2PcInstance)
 			{
@@ -323,7 +323,7 @@ public class L2BossZone extends L2ZoneType
 		if (_characterList.isEmpty())
 			return;
 		
-		for (L2Character character : _characterList.values())
+		for (L2Character character : getCharactersInsideArray())
 		{
 			if (character instanceof L2PcInstance)
 			{
@@ -384,7 +384,7 @@ public class L2BossZone extends L2ZoneType
 			return;
 		
 		Map<Integer, L2PcInstance> npcKnownPlayers = npc.getKnownList().getKnownPlayers();
-		for (L2Character character : _characterList.values())
+		for (L2Character character : getCharactersInsideArray())
 		{
 			if (character == null)
 				continue;
