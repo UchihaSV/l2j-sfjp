7cc0494ece6662a68e2d23ac1488fee8a2f9554e
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-08-20 03:06:33 +0000

    整理・微調整 「クリスマスツリー」「高級クリスマスツリー」「こたつ」

--- java/com/l2jserver/gameserver/model/actor/instance/L2XmassTreeInstance.java
+++ java/com/l2jserver/gameserver/model/actor/instance/L2XmassTreeInstance.java
@@ -19,26 +19,35 @@ import java.util.concurrent.ScheduledFuture;
 
 import com.l2jserver.gameserver.ThreadPoolManager;
 import com.l2jserver.gameserver.datatables.SkillTable;
+import com.l2jserver.gameserver.instancemanager.ZoneManager;
 import com.l2jserver.gameserver.model.L2Object;
 import com.l2jserver.gameserver.model.L2Skill;
 import com.l2jserver.gameserver.model.actor.L2Character;
 import com.l2jserver.gameserver.model.actor.L2Npc;
+import com.l2jserver.gameserver.model.zone.type.L2TownZone;
 import com.l2jserver.gameserver.network.serverpackets.ActionFailed;
 import com.l2jserver.gameserver.network.serverpackets.MagicSkillUse;
 import com.l2jserver.gameserver.templates.chars.L2NpcTemplate;
+import com.l2jserver.gameserver.templates.skills.L2SkillType;
 
 /**
  * @author Drunkard Zabb0x
  * Lets drink2code!
  * 
  * @author JOJO
- *  See also: SummonItems#useItem(), summon_items.csv
+ * 
+ * NPC          13006   クリスマス ツリー  (type: L2Npc)
+ * |アイテム    5560    クリスマス ツリー
+ * |召喚スキル  2137-1  ツリー召喚
+ * 
+ * NPC          13007   高級クリスマス ツリー  (type: L2XmassTree)
+ * |アイテム    5561    高級クリスマス ツリー
+ * |召喚スキル  2138-1  高級ツリー召喚
+ * |BUFスキル   2139-1  高級ツリーの回復ボーナス - クリスマス イベントの高級ツリー効果。HP回復ボーナス、MP回復ボーナスが向上した状態。
  */
 public class L2XmassTreeInstance extends L2Npc
 {
-	public static final int SPECIAL_TREE_ID = 13007;
-	
-	static final int[][] _buffs = {
+	private static final int[][] _buffs = {
 		 { 4262, 2 }	// クリスマス ウィンド ウォーク／一定時間、移動速度が向上した状態。効力2。
 		,{ 4263, 1 }	// クリスマス ヘイスト／一時的に攻撃速度を向上させる。効力1。
 		,{ 4264, 1 }	// クリスマス エンパワー／一時的に魔力が向上した状態。効力1。
@@ -50,39 +59,41 @@ public class L2XmassTreeInstance extends L2Npc
 
 	protected class XmassAI implements Runnable
 	{
-		private L2XmassTreeInstance _caster;
+		protected L2XmassTreeInstance _caster;
+		protected L2Skill _skill;
 		private int _buffIndex = 0;
 		
-		protected XmassAI(L2XmassTreeInstance caster/*, L2Skill skill*/)
+		protected XmassAI(L2XmassTreeInstance caster, L2Skill skill)
 		{
 			_caster = caster;
+			_skill = skill;
+			if (_skill.getSkillType() == L2SkillType.NOTDONE) throw new RuntimeException();
 		}
 		
 		public void run()
 		{
-			Collection<L2PcInstance> plrs = getKnownList().getKnownPlayersInRadius(getDistanceToWatchObject(null));
+			Collection<L2PcInstance> plrs = getKnownList().getKnownPlayersInRadius(_skill.getSkillRadius());
 			for (L2PcInstance player : plrs)
 			{
+				if (player.isInvul())
+					continue;
 				if (player.isMovementDisabled())
 					continue;
 				if (player.getPkKills() > 5)
 					continue;
-				boolean done = false;
-				if (player.getCurrentMp() < player.getMaxMp() * 8 / 10) {
-					player.setCurrentMp(player.getCurrentMp() + player.getMaxMp() / player.getLevel());
-					done = true;
-				}
-				if (player.getCurrentHp() < player.getMaxHp() * 8 / 10) {
-					player.setCurrentHp(player.getCurrentHp() + player.getMaxHp() / player.getLevel());
-					done = true;
-				}
-				if (! done && ! player.isInvul())
+				if (player.getCurrentHp() < player.getMaxHp()
+				 || player.getCurrentMp() < player.getMaxMp())
 				{
-					_buffIndex %= _buffs.length;
-					int[] b = _buffs[_buffIndex++];
+					if (player.getFirstEffect(_skill.getId()) == null)
+						_skill.getEffects(player, player);
+				}
+				else
+				{
+					int[] b = _buffs[_buffIndex];
 					handleCast(player, b[0], b[1]);
 				}
 			}
+			_buffIndex = (_buffIndex + 1) % _buffs.length;
 		}
 
 		protected boolean handleCast(L2PcInstance player, int skillId, int skillLevel)
@@ -109,9 +120,16 @@ public class L2XmassTreeInstance extends L2Npc
 	{
 		super(objectId, template);
 		setInstanceType(InstanceType.L2XmassTreeInstance);
-		if (template.npcId == SPECIAL_TREE_ID
-		  && ! isInsideZone(ZONE_PEACE) /*&& ! getIsInTown()*/)
-			_aiTask = ThreadPoolManager.getInstance().scheduleGeneralAtFixedRate(new XmassAI(this/*,SkillTable.getInstance().getInfo(2139, 1)*/), 3000, 3000);
+	}
+	
+	@Override
+	public void onSpawn()
+	{
+		super.onSpawn();
+		
+		if (getNpcId() == 13007
+				&& ZoneManager.getInstance().getZone(this, L2TownZone.class) == null)
+			_aiTask = ThreadPoolManager.getInstance().scheduleGeneralAtFixedRate(new XmassAI(this,SkillTable.getInstance().getInfo(2139, 1)), 3000, 3000);
 	}
 	
 	@Override
