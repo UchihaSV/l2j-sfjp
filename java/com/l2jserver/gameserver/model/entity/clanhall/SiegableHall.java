4401197ace8e8bbc66d19bcc87cae27e3c3f5aa6
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-09-29 07:23:36 +0000

    SYNC: CORE r4933 / BiggBoss Bandit Stronghold is now working. Core side

--- java/com/l2jserver/gameserver/model/entity/clanhall/SiegableHall.java
+++ java/com/l2jserver/gameserver/model/entity/clanhall/SiegableHall.java
@@ -222,6 +222,11 @@ public final class SiegableHall extends ClanHall
 		return getSiege().checkIsAttacker(clan);
 	}
 	
+	public SiegeStatus getSiegeStatus()
+	{
+		return _status;
+	}
+	
 	public final boolean isRegistering() 
 	{ 
 		return _status == SiegeStatus.REGISTERING; 
