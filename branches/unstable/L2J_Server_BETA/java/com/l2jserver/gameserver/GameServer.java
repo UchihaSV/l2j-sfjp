7d262367969b3e999b45fcd36d5f601ddfe16ec9
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-10-08 14:22:47 +0000

    SYNC: CORE r4967 / UnAfraid BETA: Fix for ticket #5705

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/GameServer.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/GameServer.java
@@ -251,8 +251,7 @@ public class GameServer
 		PetDataTable.getInstance();
 		CharSummonTable.getInstance().init();
 		
-		printSection("Clans");
-		CrestCache.getInstance(); // Must be initialized before ClanTable! 
+		printSection("Clans"); 
 		ClanTable.getInstance();
 		CHSiegeManager.getInstance();
 		ClanHallManager.getInstance();
@@ -299,6 +298,7 @@ public class GameServer
 		// Call to load caches
 		printSection("Cache");
 		HtmCache.getInstance();
+		CrestCache.getInstance();
 		TeleportLocationTable.getInstance();
 		UITable.getInstance();
 		PartyMatchWaitingList.getInstance();
