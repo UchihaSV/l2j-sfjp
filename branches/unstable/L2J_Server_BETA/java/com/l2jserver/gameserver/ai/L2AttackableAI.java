0b9a95795a90c7976b5f18321034f6066d8e8d2d
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2011-11-16 10:46:11 +0000

    SYNC: CORE r5086 / UnAfraid BETA: Support for effect ManaHealByLevel

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/ai/L2AttackableAI.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/ai/L2AttackableAI.java
@@ -869,16 +869,16 @@ if (com.l2jserver.Config.FIX_ATTACKABLE_AI_FACTION_CALL) {{
 					int posX = npc.getX();
 					int posY = npc.getY();
 					int posZ = npc.getZ() + 30;
-					 
-                    if (originalAttackTarget.getX() < posX)
-                        posX = posX + 300;
-                    else
-                        posX = posX - 300;
-                   
-                    if (originalAttackTarget.getY() < posY)
-                        posY = posY + 300;
-                    else
-                        posY = posY - 300;
+					
+					if (originalAttackTarget.getX() < posX)
+						posX = posX + 300;
+					else
+						posX = posX - 300;
+					
+					if (originalAttackTarget.getY() < posY)
+						posY = posY + 300;
+					else
+						posY = posY - 300;
 					
 					if (Config.GEODATA == 0 || GeoData.getInstance().canMoveFromToTarget(npc.getX(), npc.getY(), npc.getZ(), posX, posY, posZ, npc.getInstanceId()))
 						setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, new L2CharPosition(posX, posY, posZ, 0));
