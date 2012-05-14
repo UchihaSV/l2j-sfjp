e4d8747f881c799318d638e1af7a40f54304b585
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-05-14 13:56:31 +0000

    スクリプトを.zipで固めるのは廃止

--- java/com/l2jserver/gameserver/script/ScriptPackage.java
+++ java/com/l2jserver/gameserver/script/ScriptPackage.java
@@ -14,77 +14,10 @@
  */
 package com.l2jserver.gameserver.script;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.logging.Logger;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
-import javolution.util.FastList;
 
 /**
  * @author Luis Arias
  */
 public class ScriptPackage
 {
-	private static final Logger _log = Logger.getLogger(ScriptPackage.class.getName());
-	
-	private final List<ScriptDocument> _scriptFiles;
-	
-	public ScriptPackage(ZipFile pack)
-	{
-		_scriptFiles = new FastList<ScriptDocument>();
-		addFiles(pack);
-	}
-	
-	/**
-	 * @param pack
-	 */
-	private void addFiles(ZipFile pack)
-	{
-		String xml = pack.getName().replaceFirst("\\.zip$", ".xml");
-		if (new File(xml).exists())
-		{
-			try
-			{
-				InputStream in = new FileInputStream(xml);
-				ScriptDocument newScript = new ScriptDocument(xml, in);
-				_scriptFiles.add(newScript);
-				in.close();
-				System.out.println("***" + xml + " Çì«Ç›çûÇ›Ç‹ÇµÇΩÅB");
-			}
-			catch (FileNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-			return;
-		}
-		
-		for (Enumeration<? extends ZipEntry> e = pack.entries(); e.hasMoreElements();)
-		{
-			ZipEntry entry = e.nextElement();
-			if (entry.getName().endsWith(".xml"))
-			{
-				try
-				{
-					ScriptDocument newScript = new ScriptDocument(entry.getName(), pack.getInputStream(entry));
-					_scriptFiles.add(newScript);
-				}
-				catch (IOException io)
-				{
-					_log.warning(getClass().getSimpleName() + ": " + io.getMessage());
-				}
-			}
-		}
-	}
-	
 }
