543422b9f07903e1bdf8fb025f53dd05f4a21e9f
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-05-17 23:19:30 +0000

    SYNC: CORE r5318 / MELERIX BETA: A Missing SysMsg.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/network/SystemMessageId.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/network/SystemMessageId.java
@@ -13346,6 +13346,12 @@ public final class SystemMessageId
 	public static final SystemMessageId NPCS_RECAPTURED_FORTRESS;
 	
 	/**
+	 * ID: 2283<br>
+	 * Message: 座った状態では変身できません。
+	 */
+	public static final SystemMessageId CANNOT_TRANSFORM_WHILE_SITTING;
+	
+	/**
 	 * ID: 2291<br>
 	 * Message: パーティに参加した状態でのみ機械操作が可能です。
 	 */
@@ -17075,6 +17081,7 @@ public final class SystemMessageId
 		USE_SHORTCUT_CONFIRM = new SystemMessageId(2272);
 		SKILL_NOT_FOR_SUBCLASS = new SystemMessageId(2273);
 		NPCS_RECAPTURED_FORTRESS = new SystemMessageId(2276);
+		CANNOT_TRANSFORM_WHILE_SITTING = new SystemMessageId(2283);
 		CAN_OPERATE_MACHINE_WHEN_IN_PARTY = new SystemMessageId(2291);
 		LOC_IN_STEEL_CITADEL_S1_S2_S3 = new SystemMessageId(2293);
 		GAINED_VITALITY_POINTS = new SystemMessageId(2296);
