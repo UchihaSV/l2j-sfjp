8ec79a6ad5f42f0a4723ab86afa2415ed3b2db26
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-06-27 14:57:57 +0000

    SYNC: CORE r5378 / Zoey76 BETA: Changing again, hopefully will be fine now, thanks jurchiks for report.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/quest/QuestState.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/quest/QuestState.java
@@ -174,10 +174,11 @@ public final class QuestState
 		{
 			return false;
 		}
-		
+		final boolean newQuest = isCreated(); 
+		_state = state;
 		if (saveInDb)
 		{
-			if (isCreated())
+			if (newQuest)
 			{
 				Quest.createQuestInDb(this);
 			}
@@ -187,7 +188,6 @@ public final class QuestState
 			}
 		}
 		
-		_state = state;
 		_player.sendPacket(new QuestList());
 		return true;
 	}
