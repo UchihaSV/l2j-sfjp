15775a778c6773bea8a02595b1154b2d1f4f1c67
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-06-30 11:24:56 +0000

    SYNC: CORE r5394 / Adry_85 BETA: Retail-Like isMagic Support  * Using Retail-Like enums for isMagic.  * Now all Magic Skills will consume SPS/BSPS by default.  * Fixed Cast Time bonus for SPS/BSPS.  * Updated isMagic values for all skills (thx '''UnAfraid''').  * Removed isPotion & staticHitTime boolean from skills (skills with isMagic = 2 will have staticReuse & staticHitTime by default).  * Removed isDance boolean from skills (now using isMagic = 3).  * Removed nextDanceCost from Dance/Song skills (now is calculated internally). Thanks '''MELERIX''' for help.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/ai/L2AttackableAI.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/ai/L2AttackableAI.java
@@ -1923,8 +1923,8 @@ if (com.l2jserver.Config.FIX_ATTACKABLE_AI_FACTION_CALL) {{
 		{
 			return false;
 		}
-		// Is a magic skill and character is magically muted or is a physical skill and character is physically muted.
-		if (!skill.ignoreSkillMute() && ((skill.isMagic() && getActiveChar().isMuted()) || getActiveChar().isPhysicalMuted()))
+		// If is a static skill and magic skill and character is muted or is a physical skill muted and character is physically muted.
+		if (!skill.isStatic() && ((skill.isMagic() && getActiveChar().isMuted()) || getActiveChar().isPhysicalMuted()))
 		{
 			return false;
 		}
