c56907834d498b54c411aa403b69c87d79a86f47
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-07-06 08:09:50 +0000

    SYNC: CORE r5412 / MELERIX BETA: Unhardcoded and Improved Fishing Monsters by '''nonom'''.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/datatables/FishingMonstersData.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/datatables/FishingMonstersData.java
@@ -22,20 +22,20 @@ import org.w3c.dom.Node;
 
 import com.l2jserver.gameserver.engines.DocumentParser;
 import com.l2jserver.gameserver.model.StatsSet;
-import com.l2jserver.gameserver.model.fishing.L2FishingRod;
+import com.l2jserver.gameserver.model.fishing.L2FishingMonster;
 
 /**
- * This class holds the Fishing Rods information.
+ * This class holds the Fishing Monsters information.
  * @author nonom
  */
-public final class FishingRodsData extends DocumentParser
+public final class FishingMonstersData extends DocumentParser
 {
-	private static final Map<Integer, L2FishingRod> _fishingRods = new HashMap<>();
+	private static final Map<Integer, L2FishingMonster> _FishingMonsterData = new HashMap<>();
 	
 	/**
-	 * Instantiates a new fishing rods data.
+	 * Instantiates a new fishing monster data.
 	 */
-	protected FishingRodsData()
+	protected FishingMonstersData()
 	{
 		load();
 	}
@@ -43,33 +43,37 @@ public final class FishingRodsData extends DocumentParser
 	@Override
 	public void load()
 	{
-		_fishingRods.clear();
-		parseDatapackFile("data/stats/fishing/fishingRods.xml");
-		_log.info(getClass().getSimpleName() + ": Loaded " + _fishingRods.size() + " Fishing Rods.");
+		_FishingMonsterData.clear();
+		parseDatapackFile("data/stats/fishing/FishingMonsters.xml");
+		_log.info(getClass().getSimpleName() + ": Loaded " + _FishingMonsterData.size() + " Fishing Monsters.");
 	}
 	
 	@Override
 	protected void parseDocument()
 	{
+		NamedNodeMap attrs;
+		Node att;
+		L2FishingMonster fishingMonster;
+		StatsSet set;
 		for (Node n = getCurrentDocument().getFirstChild(); n != null; n = n.getNextSibling())
 		{
 			if ("list".equalsIgnoreCase(n.getNodeName()))
 			{
 				for (Node d = n.getFirstChild(); d != null; d = d.getNextSibling())
 				{
-					if ("fishingRod".equalsIgnoreCase(d.getNodeName()))
+					if ("fishingMonster".equalsIgnoreCase(d.getNodeName()))
 					{
 						
-						NamedNodeMap attrs = d.getAttributes();
+						attrs = d.getAttributes();
 						
-						StatsSet set = new StatsSet();
+						set = new StatsSet();
 						for (int i = 0; i < attrs.getLength(); i++)
 						{
-							Node att = attrs.item(i);
+							att = attrs.item(i);
 							set.set(att.getNodeName(), att.getNodeValue());
 						}
-						L2FishingRod fishingRod = new L2FishingRod(set);
-						_fishingRods.put(fishingRod.getFishingRodItemId(), fishingRod);
+						fishingMonster = new L2FishingMonster(set);
+						_FishingMonsterData.put(fishingMonster.getFishingMonsterId(), fishingMonster);
 					}
 				}
 			}
@@ -77,26 +81,33 @@ public final class FishingRodsData extends DocumentParser
 	}
 	
 	/**
-	 * Gets the fishing rod.
-	 * @param itemId the item id
-	 * @return A fishing Rod by Item Id
+	 * Gets the fishing monster.
+	 * @param lvl the fisher lvl
+	 * @return A fishing monster by fisher lvl
 	 */
-	public L2FishingRod getFishingRod(int itemId)
+	public L2FishingMonster getFishingMonster(int lvl)
 	{
-		return _fishingRods.get(itemId);
+		for (Map.Entry<Integer, L2FishingMonster> fishingMonster : _FishingMonsterData.entrySet())
+		{
+			if ((lvl >= fishingMonster.getValue().getUserMinLevel()) && (lvl <= fishingMonster.getValue().getUserMaxLevel()))
+			{
+				return fishingMonster.getValue();
+			}
+		}
+		return null;
 	}
 	
 	/**
-	 * Gets the single instance of FishingRodsData.
-	 * @return single instance of FishingRodsData
+	 * Gets the single instance of FishingMonsterData.
+	 * @return single instance of FishingMonsterData
 	 */
-	public static FishingRodsData getInstance()
+	public static FishingMonstersData getInstance()
 	{
 		return SingletonHolder._instance;
 	}
 	
 	private static class SingletonHolder
 	{
-		protected static final FishingRodsData _instance = new FishingRodsData();
+		protected static final FishingMonstersData _instance = new FishingMonstersData();
 	}
 }
