82eff4f78acf5d9851c2ca856a00164b1926aa77
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-08-18 17:47:04 +0000

    文字エンコード指定 DBDumper

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/tools/dbinstaller/util/mysql/DBDumper.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/tools/dbinstaller/util/mysql/DBDumper.java
@@ -14,9 +14,13 @@
  */
 package com.l2jserver.tools.dbinstaller.util.mysql;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -25,7 +29,6 @@ import java.util.Formatter;
 import java.util.GregorianCalendar;
 
 import com.l2jserver.tools.dbinstaller.DBOutputInterface;
-import com.l2jserver.tools.dbinstaller.util.FileWriterStdout;
 
 /**
  * @author mrTJO
@@ -46,112 +49,110 @@ public class DBDumper
 	
 	public void createDump()
 	{
-		try (Formatter form = new Formatter())
+		try (Connection con = _frame.getConnection())
 		{
-			try (Connection con = _frame.getConnection())
+			String serverVersion = "Unknown";
+			try (Statement s = con.createStatement();
+				ResultSet rset = s.executeQuery("SELECT VERSION()"))
 			{
-				String serverVersion = "Unknown";
-				try (Statement s = con.createStatement();
-					ResultSet rset = s.executeQuery("SELECT VERSION()"))
-				{
-					if (rset.next())
-						serverVersion = rset.getString(1);
-				}
-				catch (Exception e) {/*ignore error*/}
+				if (rset.next())
+					serverVersion = rset.getString(1);
+			}
+			catch (Exception e) {/*ignore error*/}
+			
+			try (Statement s = con.createStatement();
+				ResultSet rset = s.executeQuery("SHOW TABLES"))
+			{
+				Formatter form = new Formatter();
+				File dump = new File("dumps", form.format("%1$s_dump_%2$tY%2$tm%2$td-%2$tH%2$tM%2$tS.sql", _db, new GregorianCalendar().getTime()).toString());
+				form.close();
+				new File("dumps").mkdir();
+				dump.createNewFile();
 				
-				try (Statement s = con.createStatement();
-					ResultSet rset = s.executeQuery("SHOW TABLES"))
+				_frame.appendToProgressArea("Writing dump " + dump.getName());
+				if (rset.last())
 				{
-					File dump = new File("dumps", form.format("%1$s_dump_%2$tY%2$tm%2$td-%2$tH%2$tM%2$tS.sql", _db, new GregorianCalendar().getTime()).toString());
-					new File("dumps").mkdir();
-					dump.createNewFile();
-					
-					_frame.appendToProgressArea("Writing dump " + dump.getName());
-					if (rset.last())
+					int rows = rset.getRow();
+					rset.beforeFirst();
+					if (rows > 0)
 					{
-						int rows = rset.getRow();
-						rset.beforeFirst();
-						if (rows > 0)
-						{
-							_frame.setProgressIndeterminate(false);
-							_frame.setProgressMaximum(rows);
-						}
-					}
-					
-					try (FileWriter fileWriter = new FileWriter(dump);
-						FileWriterStdout fws = new FileWriterStdout(fileWriter))
-					{
-						fws.println("-- L2J DBDumper 1.0");
-						fws.println("--");
-						fws.println("-- Host: " + _host + "    Database: " + _db);
-						fws.println("-- ------------------------------------------------------");
-						fws.println("-- Server version\t	" + serverVersion);
-						fws.println();
-						fws.println("SET NAMES utf8;");
-						fws.println();
-						while (rset.next())
-						{
-							final String table = rset.getString(1);
-							_frame.setProgressValue(rset.getRow());
-							_frame.appendToProgressArea("Dumping Table " + table);
-							try (Statement desc = con.createStatement();
-								ResultSet dset = desc.executeQuery("SHOW CREATE TABLE " + table))
-							{
-								fws.println("DROP TABLE IF EXISTS `" + table + "`;");
-								while (dset.next())
-									fws.println(dset.getString(2) + ";");
-							}
-							
-							try (Statement desc = con.createStatement();
-								ResultSet dset = desc.executeQuery("SELECT * FROM " + table))
-							{
-								int cnt = 0;
-								while (dset.next())
-								{
-									if (cnt == 0)
-										fws.println("\nLOCK TABLES `" + table + "` WRITE;");
-									if (cnt % 100 == 0)
-										fws.println("INSERT INTO `" + table + "` VALUES ");
-									else
-										fws.println(",");
-									
-									fws.print("(");
-									for (int i = 1; i <= dset.getMetaData().getColumnCount(); i++)
-									{
-										final String value = dset.getString(i);
-										if (i > 1)
-											fws.print(", ");
-										
-										if (value == null)
-											fws.print("NULL");
-										else
-											fws.print("'" + value.replace("\'", "\\\'") + "'");
-									}
-									fws.print(")");
-									
-									++cnt;
-									if (cnt % 100 == 0)
-										fws.println(";");
-								}
-								if (cnt % 100 != 0)
-									fws.println(";");
-								if (cnt > 0)
-									fws.println("UNLOCK TABLES;");
-								fws.println();
-							}
-						}
-						fws.flush();
+						_frame.setProgressIndeterminate(false);
+						_frame.setProgressMaximum(rows);
 					}
 				}
+				
+				try (PrintWriter fws = new PrintWriter(new OutputStreamWriter(new FileOutputStream(dump), UTF_8)))	//[JOJO]
+				{
+					fws.println("-- L2J DBDumper 1.0");
+					fws.println("--");
+					fws.println("-- Host: " + _host + "    Database: " + _db);
+					fws.println("-- ------------------------------------------------------");
+					fws.println("-- Server version\t	" + serverVersion);
+					fws.println();
+					fws.println("SET NAMES utf8;");
+					fws.println();
+					while (rset.next())
+					{
+						final String table = rset.getString(1);
+						_frame.setProgressValue(rset.getRow());
+						_frame.appendToProgressArea("Dumping Table " + table);
+						try (Statement desc = con.createStatement();
+							ResultSet dset = desc.executeQuery("SHOW CREATE TABLE " + table))
+						{
+							fws.println("DROP TABLE IF EXISTS `" + table + "`;");
+							while (dset.next())
+								fws.println(dset.getString(2) + ";");
+						}
+						
+						try (Statement desc = con.createStatement();
+							ResultSet dset = desc.executeQuery("SELECT * FROM " + table))
+						{
+							int cnt = 0;
+							while (dset.next())
+							{
+								if (cnt == 0)
+									fws.println("\nLOCK TABLES `" + table + "` WRITE;");
+								if (cnt % 100 == 0)
+									fws.println("INSERT INTO `" + table + "` VALUES ");
+								else
+									fws.println(",");
+								
+								fws.print("(");
+								for (int i = 1; i <= dset.getMetaData().getColumnCount(); i++)
+								{
+									final String value = dset.getString(i);
+									if (i > 1)
+										fws.print(", ");
+									
+									if (value == null)
+										fws.print("NULL");
+									else
+										fws.print("'" + value.replace("\'", "\\\'") + "'");
+								}
+								fws.print(")");
+								
+								++cnt;
+								if (cnt % 100 == 0)
+									fws.println(";");
+							}
+							if (cnt % 100 != 0)
+								fws.println(";");
+							if (cnt > 0)
+								fws.println("UNLOCK TABLES;");
+							fws.println();
+						}
+					}
+					fws.flush();
+				}
 			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-			catch (SQLException e)
-			{
-				e.printStackTrace();
-			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
 		}
 		_frame.appendToProgressArea("Dump Complete!");
 	}
