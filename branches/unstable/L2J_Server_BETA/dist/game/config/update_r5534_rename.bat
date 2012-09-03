b39385837dbe77755f056631d1083b193718caba
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-09-03 23:37:42 +0000

    CORE r5534 補足

---
+++ branches/unstable/L2J_Server_BETA/dist/game/config/update_r5534_rename.bat
@@ -0,0 +1,16 @@
+REM http://trac.l2jserver.com/changeset/5534
+REN floodprotector.properties FloodProtector.properties
+REN fortsiege.properties      FortSiege.properties
+REN Grandboss.properties      GrandBoss.properties
+REN idfactory.properties      IdFactory.properties
+REN l2jmods.properties        L2JMods.properties
+REN mmo.properties            MMO.properties
+REN olympiad.properties       Olympiad.properties
+REN pvp.properties            PVP.properties
+REN rates.properties          Rates.properties
+REN security.properties       Security.properties
+REN server.properties         Server.properties
+REN server.properties.ja      Server.properties.ja
+REN siege.properties          Siege.properties
+REN telnet.properties         Telnet.properties
+REN territorywar.properties   TerritoryWar.properties
---
+++ branches/unstable/L2J_Server_BETA/dist/login/config/update_r5534_rename.bat
@@ -0,0 +1,6 @@
+REM http://trac.l2jserver.com/changeset/5534
+REN email.properties          Email.properties
+REN loginserver.properties    LoginServer.properties
+REN loginserver.properties.ja LoginServer.properties.ja
+REN mmo.properties            MMO.properties
+REN telnet.properties         Telnet.properties
