f8839d303b15d36f0b48082ce88ec3f52ffd1f3c
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-09-20 04:50:34 +0000

    手直し

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/L2Attackable.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/L2Attackable.java
@@ -1359,8 +1359,9 @@ if (com.l2jserver.Config.FIX_L2AttackableAIScript_super) {{
 			
 			double dropChance = drop.getChance();
 			
-			if (Config.RATE_DROP_ITEMS_ID.containsKey(drop.getItemId()))
-				dropChance *= Config.RATE_DROP_ITEMS_ID.get(drop.getItemId());
+			Float chance;
+			if ((chance = Config.RATE_DROP_ITEMS_ID.get(drop.getItemId())) != null)
+				dropChance *= chance;
 			else
 				dropChance *= isRaid() && !isRaidMinion() ? Config.RATE_DROP_ITEMS_BY_RAID : Config.RATE_DROP_ITEMS;
 			
