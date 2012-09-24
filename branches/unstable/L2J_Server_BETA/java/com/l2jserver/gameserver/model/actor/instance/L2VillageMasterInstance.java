459a499f09c1ca75f003bdb91b873b9a4e58a1a3
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-09-24 12:07:37 +0000

    SYNC: CORE r5584 / Zoey76 BETA: Reworking L2ClanMember related code: 	* Fix for !Noblesse/Hero status for clanless characters. Patch by: Tavo22 		* With minor changes. 	* Removed useless constructors. 	* Replaced L2ClanMember(L2Clan clan, String name, int level, int classId, int objectId, int pledgeType, int powerGrade, String title, boolean sex, int raceOrdinal) with L2ClanMember(L2Clan clan, !ResultSet clanMember). 	* Added !JavaDocs to L2ClanMember. 	* L2ClanMember#calculatePledgeClass(..) is now static. 	* L2ClanMember class format and clan up. 	* !ClanTable class format and clan up. 	* Updated other related classes.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2VillageMasterInstance.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2VillageMasterInstance.java
@@ -1030,7 +1030,7 @@ public class L2VillageMasterInstance extends L2NpcInstance
 			final L2PcInstance leaderPlayer = leaderSubPledge.getPlayerInstance();
 			if (leaderPlayer != null)
 			{
-				leaderPlayer.setPledgeClass(leaderSubPledge.calculatePledgeClass(leaderPlayer));
+				leaderPlayer.setPledgeClass(L2ClanMember.calculatePledgeClass(leaderPlayer));
 				leaderPlayer.sendPacket(new UserInfo(leaderPlayer));
 				leaderPlayer.sendPacket(new ExBrExtraUserInfo(leaderPlayer));
 			}
@@ -1114,7 +1114,7 @@ public class L2VillageMasterInstance extends L2NpcInstance
 		final L2PcInstance leaderPlayer = leaderSubPledge.getPlayerInstance();
 		if (leaderPlayer != null)
 		{
-			leaderPlayer.setPledgeClass(leaderSubPledge.calculatePledgeClass(leaderPlayer));
+			leaderPlayer.setPledgeClass(L2ClanMember.calculatePledgeClass(leaderPlayer));
 			leaderPlayer.sendPacket(new UserInfo(leaderPlayer));
 			leaderPlayer.sendPacket(new ExBrExtraUserInfo(leaderPlayer));
 		}
