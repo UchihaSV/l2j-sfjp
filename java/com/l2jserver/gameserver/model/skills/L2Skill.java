9863375f11b9d9b2bc2433201a34a3881a9cdb16
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-10-24 12:31:39 +0000

    SYNC: CORE r5663 / MELERIX BETA: CORE-Part for [DP9222], also updated charges formula.

--- java/com/l2jserver/gameserver/model/skills/L2Skill.java
+++ java/com/l2jserver/gameserver/model/skills/L2Skill.java
@@ -201,7 +201,7 @@ public abstract class L2Skill implements IChanceSkillTrigger
 	private final int _numSouls;
 	private final int _expNeeded;
 	private final int _critChance;
-	private final float _dependOnTargetBuff;
+	private final boolean _dependOnTargetBuff;
 	
 	private final int _transformId;
 	private final int _transformDuration;
@@ -513,7 +513,7 @@ public abstract class L2Skill implements IChanceSkillTrigger
 		
 		_isClanSkill = set.getBool("isClanSkill", false);
 		_excludedFromCheck = set.getBool("excludedFromCheck", false);
-		_dependOnTargetBuff = set.getFloat("dependOnTargetBuff", 0);
+		_dependOnTargetBuff = set.getBool("dependOnTargetBuff", false);
 		_simultaneousCast = set.getBool("simultaneousCast", false);
 		
 		String capsuled_items = set.getString("capsuled_items_skill", null);
@@ -2089,7 +2089,7 @@ public abstract class L2Skill implements IChanceSkillTrigger
 		return _excludedFromCheck;
 	}
 	
-	public float getDependOnTargetBuff()
+	public boolean getDependOnTargetBuff()
 	{
 		return _dependOnTargetBuff;
 	}
