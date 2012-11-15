34cdf855c15edcf0ee7f513665c1e94ddbdd1e99
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-11-15 16:17:22 +0000

    SYNC: CORE r5702 / MELERIX BETA: Eclipse is a bad guy.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/L2Character.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/L2Character.java
@@ -1819,7 +1819,7 @@ if (com.l2jserver.Config.INITIALIZE_EMPTY_COLLECTION) {{
 				skillTime = Formulas.calcAtkSpd(this, skill, skillTime);
 			}
 			// Calculate the Casting Time of Magic Skills (reduced in 40% if using SPS/BSPS)
-			if ((skill.isMagic() && isChargedShot(ShotType.SPIRITSHOTS)) || isChargedShot(ShotType.BLESSED_SPIRITSHOTS))
+			if (skill.isMagic() && (isChargedShot(ShotType.SPIRITSHOTS) || isChargedShot(ShotType.BLESSED_SPIRITSHOTS)))
 			{
 				skillTime = (int) (0.6 * skillTime);
 			}
