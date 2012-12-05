fa2b3ccca7f8aadfb930e6c86dca78b0b250369c
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2012-12-05 16:19:52 +0000

    整理

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2SepulcherMonsterInstance.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2SepulcherMonsterInstance.java
@@ -18,14 +18,11 @@ import java.util.Collection;
 import java.util.concurrent.Future;
 
 import com.l2jserver.gameserver.ThreadPoolManager;
-import com.l2jserver.gameserver.ai.CtrlEvent;
 import com.l2jserver.gameserver.ai.CtrlIntention;
-import com.l2jserver.gameserver.ai.NextAction;
 import com.l2jserver.gameserver.datatables.SkillTable;
 import com.l2jserver.gameserver.instancemanager.FourSepulchersManager;
 import com.l2jserver.gameserver.model.L2Object;
 import com.l2jserver.gameserver.model.L2Spawn;
-import com.l2jserver.gameserver.model.actor.L2Attackable;
 import com.l2jserver.gameserver.model.actor.L2Character;
 import com.l2jserver.gameserver.model.actor.templates.L2NpcTemplate;
 import com.l2jserver.gameserver.model.quest.QuestState;
