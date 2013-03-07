2233d769ebfb1f5b790235bc8b7d237c228480b8
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-03-07 14:02:22 +0000

    SYNC: CORE r5792 / Zoey76 BETA: Minor code cleanup and refactoring.

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/items/L2Item.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/items/L2Item.java
@@ -162,7 +162,7 @@ public abstract class L2Item
 	public static final int CRYSTAL_S80 = 0x06; // ??
 	public static final int CRYSTAL_S84 = 0x07; // ??
 	
-	private static final int[] crystalItemId =
+	private static final int[] CRYSTAL_ITEM_ID =
 	{
 		0,
 		1458,
@@ -173,7 +173,7 @@ public abstract class L2Item
 		1462,
 		1462
 	};
-	private static final int[] crystalEnchantBonusArmor =
+	private static final int[] CRYSTAL_ENCHANT_BONUS_ARMOR =
 	{
 		0,
 		11,
@@ -184,7 +184,7 @@ public abstract class L2Item
 		25,
 		25
 	};
-	private static final int[] crystalEnchantBonusWeapon =
+	private static final int[] CRYSTAL_ENCHANT_BONUS_WEAPON =
 	{
 		0,
 		90,
@@ -521,7 +521,7 @@ if (com.l2jserver.Config.INITIALIZE_EMPTY_COLLECTION) {{
 	 */
 	public final int getCrystalItemId()
 	{
-		return crystalItemId[_crystalType];
+		return CRYSTAL_ITEM_ID[_crystalType];
 	}
 	
 	/**
@@ -572,9 +572,9 @@ if (com.l2jserver.Config.INITIALIZE_EMPTY_COLLECTION) {{
 			{
 				case TYPE2_SHIELD_ARMOR:
 				case TYPE2_ACCESSORY:
-					return _crystalCount + (crystalEnchantBonusArmor[getCrystalType()] * ((3 * enchantLevel) - 6));
+					return _crystalCount + (CRYSTAL_ENCHANT_BONUS_ARMOR[getCrystalType()] * ((3 * enchantLevel) - 6));
 				case TYPE2_WEAPON:
-					return _crystalCount + (crystalEnchantBonusWeapon[getCrystalType()] * ((2 * enchantLevel) - 3));
+					return _crystalCount + (CRYSTAL_ENCHANT_BONUS_WEAPON[getCrystalType()] * ((2 * enchantLevel) - 3));
 				default:
 					return _crystalCount;
 			}
@@ -585,9 +585,9 @@ if (com.l2jserver.Config.INITIALIZE_EMPTY_COLLECTION) {{
 			{
 				case TYPE2_SHIELD_ARMOR:
 				case TYPE2_ACCESSORY:
-					return _crystalCount + (crystalEnchantBonusArmor[getCrystalType()] * enchantLevel);
+					return _crystalCount + (CRYSTAL_ENCHANT_BONUS_ARMOR[getCrystalType()] * enchantLevel);
 				case TYPE2_WEAPON:
-					return _crystalCount + (crystalEnchantBonusWeapon[getCrystalType()] * enchantLevel);
+					return _crystalCount + (CRYSTAL_ENCHANT_BONUS_WEAPON[getCrystalType()] * enchantLevel);
 				default:
 					return _crystalCount;
 			}
