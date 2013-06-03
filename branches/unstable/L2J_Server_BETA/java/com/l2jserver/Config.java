15899a120235ac084d687afb6781de911a4d65e4
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-06-03 02:39:19 +0000

    WalkingManager関連 * 「今後、アクティブ モンスターを刺激するおそれがあります。」の前なのに襲ってくるので修正 * 微調整 クノリクスの叫び

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/Config.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/Config.java
@@ -1201,6 +1201,7 @@ public final class Config
 	public static final boolean FIX_FleeNpc = true;						//[JOJO]反撃しないモンスター(エルピー、生贄など) TODO: UPDATE npc SET patk=0,matk=0
 	public static final boolean FIX_CoupleSocial_AI_INTENTION = true;	//[JOJO]相手が追従モードのときカップル アクションが行えない.
 	public static final boolean FIX_WALKER_ATTACK = true;				//[JOJO]WalkerManager 歩行中のモンスターを攻撃すると無反応のまま停止する不具合の修正.
+	public static final boolean PC_PROTECT = true;				//[JOJO]「今後、アクティブ モンスターを刺激するおそれがあります。」
 	
 
 	//
