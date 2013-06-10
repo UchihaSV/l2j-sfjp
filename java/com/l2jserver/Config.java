b33bb7974d5d4715146675a2d8b2fa18972ca2ff
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-06-10 14:43:55 +0000

    複数のテイミングに成功したモンスターが後をついてきたとき、重ならないように横に並ぶように・・・したい(未完成)

--- java/com/l2jserver/Config.java
+++ java/com/l2jserver/Config.java
@@ -1203,6 +1203,7 @@ public final class Config
 	public static final boolean FIX_WALKER_ATTACK = true;				//[JOJO]WalkerManager 歩行中のモンスターを攻撃すると無反応のまま停止する不具合の修正.
 	public static final boolean PC_PROTECT = true;						//[JOJO]「今後、アクティブ モンスターを刺激するおそれがあります。」
 	public static final boolean NEVER_TARGET_TAMED = true;				//[JOJO]槍・範囲攻撃をするとテイミングモンスターが巻き添えになる不具合の修正.
+	public static final boolean SORT_TAMED_BEAST = true;				//[JOJO]テイミングモンスターの位置をソート(未完成)
 	
 
 	//
