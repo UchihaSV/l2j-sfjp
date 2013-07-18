5fc6f7c73d513491dc3001efd56139bdb0c96c4b
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-07-18 02:26:07 +0000

    SYNC: CORE r5946 / Zoey76 BETA: Continuing with [5945], more core cleanup.

--- java/com/l2jserver/gameserver/model/actor/L2Character.java
+++ java/com/l2jserver/gameserver/model/actor/L2Character.java
@@ -3405,34 +3405,6 @@ if (com.l2jserver.Config.INITIALIZE_EMPTY_COLLECTION) {{
 	}
 	
 	/**
-	 * Stop a specified/all Confused abnormal L2Effect.<br>
-	 * <B><U>Actions</U>:</B>
-	 * <ul>
-	 * <li>Delete a specified/all (if effect=null) Confused abnormal L2Effect from L2Character and update client magic icon</li>
-	 * <li>Set the abnormal effect flag _confused to False</li>
-	 * <li>Notify the L2Character AI</li>
-	 * <li>Send Server->Client UserInfo/CharInfo packet</li>
-	 * </ul>
-	 * @param effect
-	 */
-	public final void stopConfused(L2Effect effect)
-	{
-		if (effect == null)
-		{
-			stopEffects(L2EffectType.CONFUSION);
-		}
-		else
-		{
-			_effectList.remove(effect);
-		}
-		if (!isPlayer())
-		{
-			getAI().notifyEvent(CtrlEvent.EVT_THINK);
-		}
-		updateAbnormalEffect();
-	}
-	
-	/**
 	 * Stop and remove the L2Effects corresponding to the L2Skill Identifier and update client magic icon.<br>
 	 * <B><U>Concept</U>:</B><br>
 	 * All active skills effects in progress on the L2Character are identified in ConcurrentHashMap(Integer,L2Effect) <B>_effects</B>.<br>
@@ -3637,19 +3609,6 @@ if (com.l2jserver.Config.INITIALIZE_EMPTY_COLLECTION) {{
 			getAI().notifyEvent(CtrlEvent.EVT_THINK);
 		}
 		updateAbnormalEffect();
-	}
-	
-	public final void stopParalyze(boolean removeEffects)
-	{
-		if (removeEffects)
-		{
-			stopEffects(L2EffectType.PARALYZE);
-		}
-		
-		if (!isPlayer())
-		{
-			getAI().notifyEvent(CtrlEvent.EVT_THINK);
-		}
 	}
 	
 	/**
