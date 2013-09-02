4a55c860caa5c2a92351cb5055af353251c08389
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-09-02 14:29:39 +0000

    境界の商人 「アカウント内のキャラクターとトッピング アイテムを共有する」 「共有アイテムを受け取る」 が動作しない

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/L2Npc.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/L2Npc.java
@@ -799,6 +799,7 @@ if (com.l2jserver.Config.NEVER_RandomAnimation_IF_DEAD) {
 	 */
 	public boolean isWarehouse()
 	{
+		if (getNpcId() == 32478) return true;	//[JOJO] 32478 境界の商人「トッピング アイテムを共有」
 		return false;
 	}
 	
