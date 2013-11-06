2391994386f6fc351377e208b4c19a58373bcf05
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-11-06 10:05:26 +0000

    SYNC: CORE r6132 / UnAfraid BETA: Implementing isInCategory method to L2Character and it's childs. 	* And minor cleanup.

--- java/com/l2jserver/gameserver/model/actor/L2Npc.java
+++ java/com/l2jserver/gameserver/model/actor/L2Npc.java
@@ -30,6 +30,7 @@ import com.l2jserver.gameserver.SevenSigns;
 import com.l2jserver.gameserver.SevenSignsFestival;
 import com.l2jserver.gameserver.ThreadPoolManager;
 import com.l2jserver.gameserver.cache.HtmCache;
+import com.l2jserver.gameserver.datatables.CategoryData;
 import com.l2jserver.gameserver.datatables.ItemTable;
 import com.l2jserver.gameserver.handler.BypassHandler;
 import com.l2jserver.gameserver.handler.IBypassHandler;
@@ -38,6 +39,7 @@ import com.l2jserver.gameserver.instancemanager.CastleManager;
 import com.l2jserver.gameserver.instancemanager.FortManager;
 import com.l2jserver.gameserver.instancemanager.TownManager;
 import com.l2jserver.gameserver.instancemanager.WalkingManager;
+import com.l2jserver.gameserver.model.CategoryType;
 import com.l2jserver.gameserver.model.L2NpcAIData;
 import com.l2jserver.gameserver.model.L2Object;
 import com.l2jserver.gameserver.model.L2Spawn;
@@ -1981,6 +1983,12 @@ if (com.l2jserver.Config.NEVER_RandomAnimation_IF_DEAD) {
 				}
 			}
 		}
+	}
+	
+	@Override
+	public boolean isInCategory(CategoryType type)
+	{
+		return CategoryData.getInstance().isInCategory(type, getNpcId());
 	}
 }
 
