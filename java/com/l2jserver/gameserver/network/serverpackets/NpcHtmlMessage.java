a5d50b111b25913e896ad1342e0ccf3ea4252089
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-12-03 12:26:30 +0000

    整理 未使用import

--- java/com/l2jserver/gameserver/network/serverpackets/NpcHtmlMessage.java
+++ java/com/l2jserver/gameserver/network/serverpackets/NpcHtmlMessage.java
@@ -19,8 +19,6 @@
 package com.l2jserver.gameserver.network.serverpackets;
 
 import com.l2jserver.gameserver.enums.BypassScope;
-import com.l2jserver.gameserver.model.L2Object;
-import com.l2jserver.gameserver.model.L2World;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 
