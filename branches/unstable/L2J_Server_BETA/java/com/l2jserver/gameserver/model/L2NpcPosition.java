6842acb45274dacbcd82f5a0dc8e3e9d710eb5c0
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2013-12-16 17:15:29 +0000

    整理

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/L2NpcPosition.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/model/L2NpcPosition.java
@@ -18,12 +18,12 @@
  */
 package com.l2jserver.gameserver.model;
 
-import com.l2jserver.gameserver.model.interfaces.IPositionable;
+import com.l2jserver.gameserver.model.interfaces.ILocational;
 
 /**
  * @author JOJO
  */
-public class L2NpcPosition implements IPositionable
+public class L2NpcPosition implements ILocational
 {
 	// This class is read only.
 	public final int npcId;
@@ -69,10 +69,4 @@ public class L2NpcPosition implements IPositionable
 	
 	@Deprecated @Override public Location getLocation() { throw new UnsupportedOperationException(); }
 	@Deprecated @Override public int getInstanceId() { throw new UnsupportedOperationException(); }
-	@Deprecated @Override public void setX(int x) { throw new UnsupportedOperationException(); }
-	@Deprecated @Override public void setY(int y) { throw new UnsupportedOperationException(); }
-	@Deprecated @Override public void setZ(int z) { throw new UnsupportedOperationException(); }
-	@Deprecated @Override public void setHeading(int heading) { throw new UnsupportedOperationException(); }
-	@Deprecated @Override public void setInstanceId(int instanceId) { throw new UnsupportedOperationException(); }
-	@Deprecated @Override public void setLocation(Location loc) { throw new UnsupportedOperationException(); }
 }
