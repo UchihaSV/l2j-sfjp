683cfa6ecb3c363c109d5571348ee7240043a42b
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2014-01-31 12:27:29 +0000

    getLocation地獄

--- branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/network/serverpackets/MagicSkillUse.java
+++ branches/unstable/L2J_Server_BETA/java/com/l2jserver/gameserver/network/serverpackets/MagicSkillUse.java
@@ -32,23 +32,29 @@ import com.l2jserver.gameserver.model.interfaces.IPositionable;
  */
 public final class MagicSkillUse extends L2GameServerPacket
 {
+	private final int _targetId, _tx, _ty, _tz;
 	private final int _skillId;
 	private final int _skillLevel;
 	private final int _hitTime;
 	private final int _reuseDelay;
-	private final L2Character _activeChar;
-	private final L2Character _target;
+	private final int _charObjId, _x, _y, _z;
 	private final List<Integer> _unknown = Collections.emptyList();
 	private final List<Location> _groundLocations;
 	
 	public MagicSkillUse(L2Character cha, L2Character target, int skillId, int skillLevel, int hitTime, int reuseDelay)
 	{
-		_activeChar = cha;
-		_target = target;
+		_charObjId = cha.getObjectId();
+		_targetId = target.getObjectId();
 		_skillId = skillId;
 		_skillLevel = skillLevel;
 		_hitTime = hitTime;
 		_reuseDelay = reuseDelay;
+		_x = cha.getX();
+		_y = cha.getY();
+		_z = cha.getZ();
+		_tx = target.getX();
+		_ty = target.getY();
+		_tz = target.getZ();
 		Location skillWorldPos = null;
 		if (cha.isPlayer())
 		{
@@ -70,13 +76,15 @@ public final class MagicSkillUse extends L2GameServerPacket
 	protected final void writeImpl()
 	{
 		writeC(0x48);
-		writeD(_activeChar.getObjectId());
-		writeD(_target.getObjectId());
+		writeD(_charObjId);
+		writeD(_targetId);
 		writeD(_skillId);
 		writeD(_skillLevel);
 		writeD(_hitTime);
 		writeD(_reuseDelay);
-		writeLoc(_activeChar);
+		writeD(_x);
+		writeD(_y);
+		writeD(_z);
 		writeH(_unknown.size()); // TODO: Implement me!
 		for (int unknown : _unknown)
 		{
@@ -87,6 +95,8 @@ public final class MagicSkillUse extends L2GameServerPacket
 		{
 			writeLoc(target);
 		}
-		writeLoc(_target);
+		writeD(_tx);
+		writeD(_ty);
+		writeD(_tz);
 	}
 }
