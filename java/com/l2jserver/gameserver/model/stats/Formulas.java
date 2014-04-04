38406a3b660d3718473b422fe6f8daaf50f30600
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2014-04-04 15:05:23 +0000

    SYNC: CORE r6362 / Nos BETA: Fixed '''debuffImmunity''' stat it wasn't being used in calcEffectSuccess formula. Reported by: facheme Tested by: St3et Reviewed by: St3et

--- java/com/l2jserver/gameserver/model/stats/Formulas.java
+++ java/com/l2jserver/gameserver/model/stats/Formulas.java
@@ -1313,6 +1313,16 @@ public final class Formulas
 		final L2Character attacker = env.getCharacter();
 		final L2Character target = env.getTarget();
 		final L2Skill skill = env.getSkill();
+		
+		if (skill.isDebuff() && (target.calcStat(Stats.DEBUFF_IMMUNITY, 0, attacker, skill) > 0))
+		{
+			final SystemMessage sm = SystemMessage.getSystemMessage(SystemMessageId.C1_RESISTED_YOUR_S2);
+			sm.addCharName(target);
+			sm.addSkillName(skill);
+			attacker.sendPacket(sm);
+			return false;
+		}
+		
 		final int activateRate = skill.getActivateRate();
 		if ((activateRate == -1) || (skill.getBasicProperty() == BaseStats.NONE))
 		{
@@ -1386,8 +1396,8 @@ public final class Formulas
 		if (finalRate <= Rnd.get(100))
 		{
 			final SystemMessage sm = SystemMessage.getSystemMessage(SystemMessageId.C1_RESISTED_YOUR_S2);
-			sm.addCharName(env.getTarget());
-			sm.addSkillName(env.getSkill());
+			sm.addCharName(target);
+			sm.addSkillName(skill);
 			attacker.sendPacket(sm);
 			return false;
 		}
