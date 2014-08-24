fe2b5d9c563554d762b896ad98214c8f33256d86
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2014-08-24 05:16:32 +0000

    SYNC: CORE r6396 / Nos BETA: Reworked `DecayTaskManager`. 	* Changed "too old corpse" condition on retail its 2 seconds before corpse disappears.
    Reviewed by: !UnAfraid, !Adry_85

--- java/com/l2jserver/gameserver/model/actor/instance/L2TrapInstance.java
+++ java/com/l2jserver/gameserver/model/actor/instance/L2TrapInstance.java
@@ -374,7 +374,7 @@ public final class L2TrapInstance extends L2Npc
 	
 	public void stopDecay()
 	{
-		DecayTaskManager.getInstance().cancelDecayTask(this);
+		DecayTaskManager.getInstance().cancel(this);
 	}
 	
 	/**
