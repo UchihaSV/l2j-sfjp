573a0d6fb8a48f830449fec631e2917dc9b3e23a
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2014-10-19 17:25:46 +0000

    SYNC: CORE r6463 / Zoey76 BETA: Implementing Initial Shortcuts: 	* Allows customization of initial shortcuts and macros for new characters. Reviewed by: Nos, St3eT, UnAfraid

--- java/com/l2jserver/gameserver/GameServer.java
+++ java/com/l2jserver/gameserver/GameServer.java
@@ -65,6 +65,7 @@ import com.l2jserver.gameserver.datatables.FishingRodsData;
 import com.l2jserver.gameserver.datatables.HennaData;
 import com.l2jserver.gameserver.datatables.HitConditionBonus;
 import com.l2jserver.gameserver.datatables.InitialEquipmentData;
+import com.l2jserver.gameserver.datatables.InitialShortcutData;
 import com.l2jserver.gameserver.datatables.ItemTable;
 import com.l2jserver.gameserver.datatables.KarmaData;
 import com.l2jserver.gameserver.datatables.ManorData;
@@ -255,6 +256,7 @@ public class GameServer
 		printSection("Characters");
 		ClassListData.getInstance();
 		InitialEquipmentData.getInstance();
+		InitialShortcutData.getInstance();
 		ExperienceTable.getInstance();
 		KarmaData.getInstance();
 		HitConditionBonus.getInstance();
