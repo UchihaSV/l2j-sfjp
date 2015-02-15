d03fe4b7577c056cc1aedcccefd13504de20d095
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2015-02-15 05:02:45 +0000

    整理 ConcurrentFastMap

--- java/com/l2jserver/util/ConcurrentFastMap.java
+++ java/com/l2jserver/util/ConcurrentFastMap.java
@@ -4,7 +4,7 @@
  */
 package com.l2jserver.util;
 
-import java.util.concurrent.ConcurrentMap;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -13,68 +13,163 @@ import javolution.util.FastMap;
 
 @SuppressWarnings("serial")
 public class ConcurrentFastMap<K, V> extends FastMap<K, V>
-	implements ConcurrentMap<K, V>
+    /*implements ConcurrentMap<K, V>*/
 {
-	public ConcurrentFastMap()
-	{
-		shared();
-	}
+    public ConcurrentFastMap()
+    {
+        shared();
+    }
 
-	@Override
-	public boolean remove(Object key, Object value)
-	{
-		throw new UnsupportedOperationException();	// TODO:
-	}
+    @Override
+    public V getOrDefault(Object key, V defaultValue) {
+        V v;
+        return ((v = get(key)) != null) ? v : defaultValue;
+    }
 
-	@Override
-	public boolean replace(K key, V oldValue, V newValue)
-	{
-		throw new UnsupportedOperationException();	// TODO:
-	}
+    @Override
+    public void forEach(BiConsumer<? super K, ? super V> action) {
+        if (action == null) throw new NullPointerException();
+        for (java.util.Map.Entry<K, V> entry : entrySet())
+            if (entry != null)
+                action.accept(entry.getKey(), entry.getValue());
+    }
 
-	@Override
-	public V replace(K key, V value)
-	{
-		throw new UnsupportedOperationException();	// TODO:
-	}
-	
-	@Override
-	public void forEach(BiConsumer<? super K, ? super V> action) {
-		if (action == null) throw new NullPointerException();
-		for (java.util.Map.Entry<K, V> entry : entrySet())
-			if (entry != null)
-				action.accept(entry.getKey(), entry.getValue());
-	}
-	
-	@Override
-	public V computeIfAbsent(K key, Function<? super K, ? extends V> function) {
-		if (function == null) throw new NullPointerException();
-		V oldValue;
-		if ((oldValue = get(key)) != null) return oldValue;
-		synchronized (this) {
-			if ((oldValue = get(key)) != null) return oldValue;
-			V newValue = function.apply(key);	// -> new V
-			if (newValue != null)
-		//TODO:	FastMap#put(key, newValue, _isDirectKeyComparator ? key.hashCode() : _keyComparator.hashCodeOf(key), false , false, false); private --> protected
-				put(key, newValue);	// maybe synchronized double.
-			return newValue;
-		}
-	}
-	
-	@Override
-	public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> function) {
-		if (function == null) throw new NullPointerException();
-		javolution.util.FastMap.Entry<K, V> entry;
-		if ((entry = getEntry(key)) == null) return null;
-		synchronized (this) {
-			if ((entry = getEntry(key)) == null) return null;
-			V newValue = function.apply(key, entry.getValue());	// -> new V
-			if (newValue != null)
-				entry.setValue(newValue);
-			else
-		//TODO:	FastMap#remove(key, false); private --> protected
-				remove(key);	// maybe synchronized double.
-			return newValue;
-		}
-	}
+   /* --> FastMap.putIfAbsent
+    V putIfAbsent(K key, V value);
+    */
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean remove(Object key, Object value) {
+        FastMap.Entry<K, V> entry = getEntry(key);
+        if (entry == null || !getValueComparator().areEqual(entry.getValue(), (V) value))	// Warning "unchecked"
+            return false;
+        remove(key);
+        return true;
+    }
+
+    @Override
+    public boolean replace(K key, V oldValue, V newValue) {
+        FastMap.Entry<K, V> entry = getEntry(key);
+        if (entry == null || !getValueComparator().areEqual(entry.getValue(), oldValue)) return false;
+        entry.setValue(newValue);
+        return true;
+    }
+
+    @Override
+    public V replace(K key, V value) {
+        FastMap.Entry<K, V> entry = getEntry(key);
+        if (entry == null) return null;
+        return entry.setValue(value);
+    }
+
+    @Override
+    public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+        Objects.requireNonNull(function);
+        forEach((k,v) -> {
+            while(!replace(k, v, function.apply(k, v))) {
+                // v changed or k is gone
+                if ( (v = get(k)) == null) {
+                    // k is no longer in the map.
+                    break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public V computeIfAbsent(K key,
+            Function<? super K, ? extends V> mappingFunction) {
+        Objects.requireNonNull(mappingFunction);
+        V v, newValue;
+        return ((v = get(key)) == null &&
+                (newValue = mappingFunction.apply(key)) != null &&
+                (v = putIfAbsent(key, newValue)) == null) ? newValue : v;
+    }
+
+    @Override
+    public V computeIfPresent(K key,
+            BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+        Objects.requireNonNull(remappingFunction);
+        V oldValue;
+        while((oldValue = get(key)) != null) {
+            V newValue = remappingFunction.apply(key, oldValue);
+            if (newValue != null) {
+                if (replace(key, oldValue, newValue))
+                    return newValue;
+            } else if (remove(key, oldValue))
+               return null;
+        }
+        return oldValue;
+    }
+
+    @Override
+    public V compute(K key,
+            BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+        Objects.requireNonNull(remappingFunction);
+        V oldValue = get(key);
+        for(;;) {
+            V newValue = remappingFunction.apply(key, oldValue);
+            if (newValue == null) {
+                // delete mapping
+                if (oldValue != null || containsKey(key)) {
+                    // something to remove
+                    if (remove(key, oldValue)) {
+                        // removed the old value as expected
+                        return null;
+                    }
+
+                    // some other value replaced old value. try again.
+                    oldValue = get(key);
+                } else {
+                    // nothing to do. Leave things as they were.
+                    return null;
+                }
+            } else {
+                // add or replace old mapping
+                if (oldValue != null) {
+                    // replace
+                    if (replace(key, oldValue, newValue)) {
+                        // replaced as expected.
+                        return newValue;
+                    }
+
+                    // some other value replaced old value. try again.
+                    oldValue = get(key);
+                } else {
+                    // add (replace if oldValue was null)
+                    if ((oldValue = putIfAbsent(key, newValue)) == null) {
+                        // replaced
+                        return newValue;
+                    }
+
+                    // some other value replaced old value. try again.
+                }
+            }
+        }
+    }
+
+    @Override
+    public V merge(K key, V value,
+            BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+        Objects.requireNonNull(remappingFunction);
+        Objects.requireNonNull(value);
+        V oldValue = get(key);
+        for (;;) {
+            if (oldValue != null) {
+                V newValue = remappingFunction.apply(oldValue, value);
+                if (newValue != null) {
+                    if (replace(key, oldValue, newValue))
+                        return newValue;
+                } else if (remove(key, oldValue)) {
+                    return null;
+                }
+                oldValue = get(key);
+            } else {
+                if ((oldValue = putIfAbsent(key, value)) == null) {
+                    return value;
+                }
+            }
+        }
+    }
 }
