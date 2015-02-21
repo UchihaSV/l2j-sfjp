59585527e91a349ea4c11902a9fcc8068effa8c3
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2015-02-21 15:51:46 +0000

    微調整 log ディレクトリに *.lck ファイルが消えずに残る. * L2LogManager は reset() を殺しているので.

--- dist/game/startGameServer.bat
+++ dist/game/startGameServer.bat
@@ -5,7 +5,9 @@ title Game Server Console
 echo Starting L2J Game Server.
 echo.
 
-java -Djava.util.logging.manager=com.l2jserver.util.L2LogManager -Xms1024m -Xmx1536m -cp ./../libs/*;l2jserver.jar com.l2jserver.gameserver.GameServer
+java -Xms1024m -Xmx1536m -cp ./../libs/*;l2jserver.jar com.l2jserver.gameserver.GameServer
+
+REM -Djava.util.logging.manager=com.l2jserver.util.L2LogManager
 
 REM NOTE: If you have a powerful machine, you could modify/add some extra parameters for performance, like:
 REM -Xms1536m
