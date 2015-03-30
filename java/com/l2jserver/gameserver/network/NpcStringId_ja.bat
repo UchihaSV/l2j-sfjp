e9ec62fd756c926bb5481c0d9f7a8b514c6f9187
torikawatukune <torikawatukune@7d3f705e-0b0b-4019-9fd5-072e50595f16>
2015-03-30 23:42:23 +0000

    整理 NpcStringId、SystemMessageId

--- java/com/l2jserver/gameserver/network/NpcStringId_ja.bat
+++ java/com/l2jserver/gameserver/network/NpcStringId_ja.bat
@@ -1,8 +1,20 @@
 CD /D %~dp0
 svn revert NpcStringId.java ||@(PAUSE&GOTO :EOF)
+@SET REV=
+@FOR /F usebackq %%I IN (`svnversion -n NpcStringId.java`) DO SET REV=%%I
+@ECHO [%REV%]
+@ECHO ### Eclipse Workspace Patch 1.0>###.patch
+@ECHO #P L2J_Server_BETA>>###.patch
 
 PUSHD ..\..\..\..\..\
-svn patch java\com\l2jserver\gameserver\network\NpcStringId_ja.diff ||@(PAUSE&GOTO :EOF)
+svn patch java\com\l2jserver\gameserver\network\NpcStringId_ja.patch ||@(PAUSE&GOTO :EOF)
+svn diff  java\com\l2jserver\gameserver\network\NpcStringId.java>>java\com\l2jserver\gameserver\network\###.patch
 POPD
 
 perl NpcStringId_ja.pl ||@(PAUSE&GOTO :EOF)
+
+fc /L  ###.patch NpcStringId_ja.patch || (
+RENAME NpcStringId_ja.patch NpcStringId_ja.patch.%REV%.BAK
+COPY   ###.patch NpcStringId_ja.patch
+RENAME ###.patch NpcStringId_ja.patch.#r%REV%
+)
